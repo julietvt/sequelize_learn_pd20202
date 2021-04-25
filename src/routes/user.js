984d88e36e3d4227c4f5e06f496bd81615c82c7d
julietvt <freshitpt@gmail.com>
2021-04-25 13:02:08 +0300

    user router was updated

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,31 @@
 import express from 'express';
-import { createUser } from '../controllers/user';
-import validateUser from '../middlewares/user/validateUser.js';
+import {
+  createUser,
+  updateUserByPk,
+  getUserByPk,
+  deleteUserByPk,
+} from '../controllers/user';
+import validationMV from '../middlewares/validation/validationMV.js';
+import { ACTION } from '../constants';
+import schemas from '../utils/validation';
+
 const userRouter = express.Router();
-userRouter.post('/', validateUser, createUser);
+const createUserValidationMV = validationMV(schemas.userSchema);
+
+userRouter.post('/', createUserValidationMV(ACTION.CREATE), createUser);
+
+userRouter.patch(
+  '/:userId',
+  createUserValidationMV(ACTION.UPDATE),
+  updateUserByPk
+);
+
+userRouter.get('/:userId', createUserValidationMV(ACTION.READ), getUserByPk);
+
+userRouter.delete(
+  '/:userId',
+  createUserValidationMV(ACTION.DELETE),
+  deleteUserByPk
+);
+
 export default userRouter;
