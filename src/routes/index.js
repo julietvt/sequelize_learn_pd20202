4a2937ed154f2a961a3617fc5d42eb59e8857284
julietvt <freshitpt@gmail.com>
2021-04-25 13:08:28 +0300

    common router was updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import userRouter from './user.js';
+import taskRouter from './task.js';
 import errorHandler from '../middlewares/errorHandler.js';
+
 const router = express.Router();
+
 router.use('/user', userRouter);
+
+router.use('/task', taskRouter);
+
 router.use(errorHandler);
+
 export default router;
